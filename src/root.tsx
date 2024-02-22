3fe35f41a0902e0b6783feb9a33441ac902a6a60
taichi221228 <taichi221228@icloud.com>
2024-02-22 18:08:41 +0900

    refactor: Move RouterHead to components directory
    The RouterHead component was previously located in 'src/functional/router-head'. It has now been moved to 'src/components/functional/router-head'. This commit updates the path in the corresponding import statement in 'src/root.tsx' to reflect this change.

--- src/root.tsx
+++ src/root.tsx
@@ -4,7 +4,7 @@ import {
   RouterOutlet,
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
-import { RouterHead } from "~/functional/router-head/router-head";
+import { RouterHead } from "~/components/functional/router-head/router-head";
 
 import "./global.css";
 
