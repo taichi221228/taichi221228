431ee9c5db9368a61b3e27a62e6a73e37789361f
taichi221228 <taichi221228@icloud.com>
2024-02-24 15:44:54 +0900

    feat: Use unocss/reset in layout.tsx and remove body margin in CSS
    The layout.tsx file has been updated to utilize unocss/reset system, by importing it and using useStyles$ feature. The body margin in global.css has also been removed to create a more simplified layout and to provide better cross-browser compatibility.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,12 @@
-import { component$, Slot } from "@builder.io/qwik";
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+
+import styles from "@unocss/reset/tailwind.css?inline";
 
 import { Footer } from "~/components/ui/footer/footer";
 import { Header } from "~/components/ui/header/header";
 
 export default component$(() => {
+  useStyles$(styles);
   return (
     <>
       <Header />
