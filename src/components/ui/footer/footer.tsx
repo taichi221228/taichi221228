811fbd39b08755c3b85ec1a6bb49302429a01f96
taichi221228 <taichi221228@icloud.com>
2024-02-24 18:30:19 +0900

    feat: Update Footer UI and Add SNS Links
    Enhanced UI styling of the footer module and added links to social networking services (SNS). The styling changes include adjustments to layout, font size, color, and borders. Social media links for Twitter, Facebook, and Github have been provided for better user interaction and accessibility.

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -2,6 +2,42 @@ import { component$ } from "@builder.io/qwik";
 
 import styles from "./footer.module.css";
 
+const username = "taichi221228";
+
+type Sns = Record<
+  string,
+  {
+    username: string;
+    url: string;
+  }
+>;
+
+const sns = {
+  twitter: {
+    username,
+    url: `https://x.com/${username}`,
+  },
+  facebook: {
+    username,
+    url: `https://www.facebook.com/${username}`,
+  },
+  github: {
+    username,
+    url: `https://github.com/${username}`,
+  },
+} as const satisfies Sns;
 export const Footer = component$(() => {
-  return <footer class={styles.footer}></footer>;
+  return (
+    <footer class={styles.footer}>
+      <p>find me in:</p>
+      {Object.entries(sns).map(([name, profile]) => {
+        return (
+          <a key={name} href={profile.url} target="_blank" class={styles[name]}>
+            {name === "github" && `@${profile.username}`}
+            <i class={`ri-${name}-fill`}></i>
+          </a>
+        );
+      })}
+    </footer>
+  );
 });
