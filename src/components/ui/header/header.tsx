93f05910a0b23734b43a6bf34304be022d3dfb1e
taichi221228 <taichi221228@icloud.com>
2024-02-24 15:37:40 +0900

    feat: Add CSS module to style Header component
    A new CSS module, named 'header.module.css', has been added to style the Header component. This style is then applied within the Header component to achieve consistent rendering across instances.

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,5 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
+import styles from "./header.module.css";
+
 export const Header = component$(() => {
-  return <header>Header</header>;
+  return <header class={styles.header}>Header</header>;
 });
