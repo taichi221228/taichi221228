60f3f1376b7b5c339d4e6367d5b8cdcd4f1b4ce7
taichi221228 <taichi221228@icloud.com>
2024-02-24 16:05:23 +0900

    feat: Implement responsive header design and navigation
    The header component has been redesigned with a flexible display, and a new navigation bar has been added. This enhancement includes more organized styles by applying some CSS properties, and a dynamic list of navigation items in the JavaScript file. These changes improve the user interface and user experience.

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -2,6 +2,23 @@ import { component$ } from "@builder.io/qwik";
 
 import styles from "./header.module.css";
 
+const items = ["hello", "about-me", "projects", "blog", "contact-me"];
+
 export const Header = component$(() => {
-  return <header class={styles.header}>Header</header>;
+  return (
+    <header class={styles.header}>
+      <h1 class={styles.title}>taichi-fukuda</h1>
+      <nav class={styles.navigation}>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li class={styles.li} key={item}>
+                <a href={`/${item === "hello" ? "" : item}`}>_{item}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
 });
