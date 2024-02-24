bcd146bb63a4ea5f59436d3788971ec453826cf3
taichi221228 <taichi221228@icloud.com>
2024-02-24 17:52:19 +0900

    feat: Add locked state to header navigation items
    The change introduces a locked state to the navigation items in the header. This visual cue is set by a change in CSS and serves to inhibit user interaction with the active navigation link, thereby improving the user interface through a clear indicator of the currently active page.

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,19 +1,27 @@
 import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 
 import styles from "./header.module.css";
 
 const items = ["hello", "about-me", "projects", "blog", "contact-me"];
 
 export const Header = component$(() => {
+  const location = useLocation();
+
   return (
     <header class={styles.header}>
       <h1 class={styles.title}>taichi-fukuda</h1>
       <nav class={styles.navigation}>
         <ul>
           {items.map((item) => {
+            const pathname = `/${item === "hello" ? "" : item}`;
+            const isCurrent = pathname === location.url.pathname;
+
             return (
-              <li class={styles.li} key={item}>
-                <a href={`/${item === "hello" ? "" : item}`}>_{item}</a>
+              <li key={item} class={isCurrent ? styles.locked : ""}>
+                <a href={pathname} tabIndex={isCurrent ? -1 : 0}>
+                  _{item}
+                </a>
               </li>
             );
           })}
