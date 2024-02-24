f2c878ccb6c765dd4c943bf5a003c08dd11666d3
taichi221228 <taichi221228@icloud.com>
2024-02-24 19:15:24 +0900

    refactor: Add SNS constant and refactor footer component
    A new Social Network Service (SNS) constant was added into the `info.ts` file to store user's SNS information in a structured manner. The `Footer` component was refactored to import and use this new SNS constant, replacing previously hard-coded SNS details, making code maintenance more manageable and improving scalability.

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,36 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 
+import { SNS } from "~/constants/info";
+
 import styles from "./footer.module.css";
 
-const username = "taichi221228";
-
-type Sns = Record<
-  string,
-  {
-    username: string;
-    url: string;
-  }
->;
-
-const sns = {
-  twitter: {
-    username,
-    url: `https://x.com/${username}`,
-  },
-  facebook: {
-    username,
-    url: `https://www.facebook.com/${username}`,
-  },
-  github: {
-    username,
-    url: `https://github.com/${username}`,
-  },
-} as const satisfies Sns;
 export const Footer = component$(() => {
   return (
     <footer class={styles.footer}>
       <p>find me in:</p>
-      {Object.entries(sns).map(([name, profile]) => {
+      {Object.entries(SNS).map(([name, profile]) => {
         return (
           <a key={name} href={profile.url} target="_blank" class={styles[name]}>
             {name === "github" && `@${profile.username}`}
--- src/constants/info.ts
+++ src/constants/info.ts
@@ -1,2 +1,25 @@
 export const TITLE = "taichi-fukuda";
 export const USERNAME = "taichi221228";
+
+type Sns = Record<
+  string,
+  {
+    username: string;
+    url: string;
+  }
+>;
+
+export const SNS = {
+  twitter: {
+    username: USERNAME,
+    url: `https://x.com/${USERNAME}`,
+  },
+  facebook: {
+    username: USERNAME,
+    url: `https://www.facebook.com/${USERNAME}`,
+  },
+  github: {
+    username: USERNAME,
+    url: `https://github.com/${USERNAME}`,
+  },
+} as const satisfies Sns;
