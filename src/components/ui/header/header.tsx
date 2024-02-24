2f45d9b0271c26bdc29db69f8b83f52d74967959
taichi221228 <taichi221228@icloud.com>
2024-02-24 19:03:49 +0900

    feat: Implement Conditional Main Title Link
    A title variable has been added and functionality to compare paths have been incorporated in the header component. Now, the main title will be a clickable link leading to the root ("/") unless the location is already at the root. This enhances user navigation on the website.

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -3,19 +3,22 @@ import { useLocation } from "@builder.io/qwik-city";
 
 import styles from "./header.module.css";
 
+const title = "taichi-fukuda";
 const items = ["hello", "about-me", "projects", "blog", "contact-me"];
 
 export const Header = component$(() => {
   const location = useLocation();
+  const comparePath = (pathname: string) => pathname === location.url.pathname;
+  const isRoot = comparePath("/");
 
   return (
     <header class={styles.header}>
-      <h1 class={styles.title}>taichi-fukuda</h1>
+      <h1 class={styles.title}>{isRoot ? title : <a href="/">{title}</a>}</h1>
       <nav class={styles.navigation}>
         <ul>
           {items.map((item) => {
             const pathname = `/${item === "hello" ? "" : item}`;
-            const isCurrent = pathname === location.url.pathname;
+            const isCurrent = comparePath(pathname);
 
             return (
               <li key={item} class={isCurrent ? styles.locked : ""}>
