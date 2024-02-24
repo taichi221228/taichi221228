eaac08f07b2bfb1d59fe4d505df870bfc7b395db
taichi221228 <taichi221228@icloud.com>
2024-02-24 18:29:31 +0900

    feat: Integrate remixicon styles into layout component
    Two separate stylesheets (resetStyles and iconStyles) are imported and used in the component for improved style handling. This enhances the visual aspect and style consistency across the site, specifically using Remixicon styles for icons.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,12 +1,15 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 
-import styles from "@unocss/reset/tailwind.css?inline";
+import resetStyles from "@unocss/reset/tailwind.css?inline";
+import iconStyles from "remixicon/fonts/remixicon.css?inline";
 
 import { Footer } from "~/components/ui/footer/footer";
 import { Header } from "~/components/ui/header/header";
 
 export default component$(() => {
-  useStyles$(styles);
+  useStyles$(resetStyles);
+  useStyles$(iconStyles);
+
   return (
     <>
       <Header />
