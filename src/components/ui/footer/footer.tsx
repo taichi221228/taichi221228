46a208f70900f2a020cba567c99859da7393e7dc
taichi221228 <taichi221228@icloud.com>
2024-02-24 18:09:39 +0900

    feat: Add new footer component and corresponding CSS module
    A new footer component has been added to the UI components. Along with it, a corresponding CSS module has also been created. The new component structure improves code organization and allows for dedicated styling.

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,5 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
+import styles from "./footer.module.css";
+
 export const Footer = component$(() => {
-  return <footer>Footer</footer>;
+  return <footer class={styles.footer}></footer>;
 });
