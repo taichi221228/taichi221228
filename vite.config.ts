73780974ce72fcb0c73c8ebb16c765e7150d93bd
taichi221228 <taichi221228@icloud.com>
2024-02-24 09:57:35 +0900

    chore: Add PandaCSS configuration and update Vite plugins
    Created a new configuration file for PandaCSS, added the PandaCSS dev/plugin to postCSS, and updated the plugins in the Vite configuration. These changes ensure a more efficient integration of PandaCSS and provide more robust and customizable styling options.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { macroPlugin } from "@builder.io/vite-plugin-macro";
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      macroPlugin({ preset: "pandacss" }),
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+    ],
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
