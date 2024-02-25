21a13c386dced34ed618b90289621f5899241387
taichi221228 <taichi221228@icloud.com>
2024-02-25 12:26:26 +0900

    feat: Add comment blocks and styles to description section
    Added two comment blocks to the description section in index route, offering directions for gameplay and information about our Github page. Enhancements were also made in the top-page CSS module to improve visual appeal and layout of these comments.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,7 +16,20 @@ export default component$(() => {
             <h3>&gt; Front-end developer</h3>
           </div>
           <div class={styles.game}></div>
-          <div class={styles.description}></div>
+          <div class={styles.description}>
+            <p class={styles.comment}>
+              <span class={styles.line}>//&nbsp;</span>
+              <span class={styles.block}>/*&nbsp;</span>
+              complete the game to continue
+              <span class={styles.block}>&nbsp;*/</span>
+            </p>
+            <p class={styles.comment}>
+              <span class={styles.line}>//&nbsp;</span>
+              <span class={styles.block}>/*&nbsp;</span>
+              you can also see it on my Github page
+              <span class={styles.block}>&nbsp;*/</span>
+            </p>
+          </div>
         </div>
       </div>
     </section>
