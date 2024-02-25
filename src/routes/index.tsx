d4fbe15cf9b76a0b81b9f201077feb78fc93931c
taichi221228 <taichi221228@icloud.com>
2024-02-25 12:20:28 +0900

    feat: Add styling to section in Top-page
    Updated both index.tsx and top-page.module.css files. In index.tsx, classes for container and wrapper are imported from top-page.module.css. In top-page.module.css, styling rules are added for more complex layout including grid structure and responsive padding.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,16 @@ import { type DocumentHead } from "@builder.io/qwik-city";
 
 import { TITLE } from "~/constants/info";
 
-import styles from "./top-page.module.css"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import styles from "./top-page.module.css";
 
 export default component$(() => {
-  return <section></section>;
+  return (
+    <section>
+      <div class={styles.wrapper}>
+        <div class={styles.container}></div>
+      </div>
+    </section>
+  );
 });
 
 export const head: DocumentHead = {
