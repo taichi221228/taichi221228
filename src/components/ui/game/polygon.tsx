f121b2fd64eed8b0e27ab9d67236243c91797bdc
taichi221228 <taichi221228@icloud.com>
2024-02-26 16:28:09 +0900

    feat: Updated polygon styles and SVG paths in game UI
    Added blur and opacity effects to the Polygon component, enhancing its visual appeal. Added two separate SVG path definitions to display distinct polygon shapes, helping to differentiate them visually. Updated color properties in the module's CSS to allow a harmonious application of color, corresponding with the rest of the user interface.

--- src/components/ui/game/polygon.tsx
+++ src/components/ui/game/polygon.tsx
@@ -1,7 +1,24 @@
 import { component$ } from "@builder.io/qwik";
 
-import styles from "./polygon.module.css"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import styles from "./polygon.module.css";
 
 export const Polygon = component$(() => {
-  return <></>;
+  const blurMargin = 500;
+  const [polygonWidth, polygonHeight] = [780, 681];
+  const [width, height] = [
+    polygonWidth + blurMargin * 2,
+    polygonHeight + blurMargin * 2,
+  ];
+  const viewBox = [blurMargin * -1, blurMargin * -1, width, height].join(" ");
+
+  return (
+    <svg width={width} height={height} viewBox={viewBox} fill="none">
+      <g class={styles.polygon1}>
+        <path d="M549.946 680.705L336.032 629.318L246.663 564.654L273.226 394.142L448.649 374.576L447.041 252.833L649.309 198.577L779.602 229.876L697.165 495.982L569.892 529.172L549.946 680.705Z" />
+      </g>
+      <g class={styles.polygon2}>
+        <path d="M499.469 83.6671L515.984 303.046L481.759 407.911L311.295 434.783L239.003 273.755L123.588 312.528L10.0585 136.554L-5.74887e-05 2.93161L278.567 0.012974L349.099 111.032L499.469 83.6671Z" />
+      </g>
+    </svg>
+  );
 });
