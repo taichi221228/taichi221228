583c495d79e7f7951c01e152ee3137a6e66e426d
taichi221228 <taichi221228@icloud.com>
2024-02-26 14:53:51 +0900

    refactor: Remove ESLint disable-line in game component
    Previously, the "no-unused-vars" rule for ESLint was disabled for the game component. This has now been removed, ensuring better adherence to linting standards in the codebase. This should improve code maintainability and consistency across the project.

--- src/components/ui/game/game.tsx
+++ src/components/ui/game/game.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 
-import styles from "./game.module.css"; // eslint-disable-line @typescript-eslint/no-unused-vars
+import styles from "./game.module.css";
 
 export const Game = component$(() => {
   return <div class={styles.container}></div>;
