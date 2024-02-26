db42da04537f8db1af866627b24241cfb53ffd8d
taichi221228 <taichi221228@icloud.com>
2024-02-26 14:55:02 +0900

    feat: Add Polygon component to Game module
    A new component, Polygon, has been created and added to the Game module. Game.module.css has been updated to support this addition. The component has been imported and utilized in the Game component, enhancing the modularity and reusability of the code.

--- src/components/ui/game/game.tsx
+++ src/components/ui/game/game.tsx
@@ -1,7 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 
 import styles from "./game.module.css";
+import { Polygon } from "./polygon";
 
 export const Game = component$(() => {
-  return <div class={styles.container}></div>;
+  return (
+    <div class={styles.container}>
+      <div class={styles.polygon}>
+        <Polygon />
+      </div>
+    </div>
+  );
 });
---
+++ src/components/ui/game/polygon.tsx
@@ -0,0 +1,7 @@
+import { component$ } from "@builder.io/qwik";
+
+import styles from "./polygon.module.css"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export const Polygon = component$(() => {
+  return <></>;
+});
