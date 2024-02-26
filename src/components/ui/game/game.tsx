e92117f62ac91b957d894ba1e156a5c974fd4b37
taichi221228 <taichi221228@icloud.com>
2024-02-26 14:38:49 +0900

    feat: Create new "Game" component and style file
    The commit introduces a new "Game" component (`game.tsx`) and corresponding CSS module (`game.module.css`). This will enhance the application's structure by encapsulating game-specific code in a separate module, thus improving code readability and maintainability.

---
+++ src/components/ui/game/game.tsx
@@ -0,0 +1,7 @@
+import { component$ } from "@builder.io/qwik";
+
+import styles from "./game.module.css"; // eslint-disable-line @typescript-eslint/no-unused-vars
+
+export const Game = component$(() => {
+  return <></>;
+});
