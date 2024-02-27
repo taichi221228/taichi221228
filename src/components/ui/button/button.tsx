945179822a2d656da112bd6595a2135905bc551d
taichi221228 <taichi221228@icloud.com>
2024-02-27 14:55:31 +0900

    feat: Update Button component to handle onClick and variant props
    Implemented onClick event handler and added variant prop for different button types in Button component. Modified corresponding CSS styles to support new 'primary', 'accent' and 'ghost' variants. These changes allow for greater customization and interactivity of Buttons within the application.

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,10 +1,15 @@
-import { component$, Slot } from "@builder.io/qwik";
+import { type QRL, component$, Slot } from "@builder.io/qwik";
 
 import styles from "./button.module.css";
 
-export const Button = component$(() => {
+type Props = {
+  onClick$: QRL<() => void>;
+  variant?: "primary" | "accent" | "ghost";
+};
+
+export const Button = component$<Props>(({ onClick$, variant = "primary" }) => {
   return (
-    <button class={styles.button}>
+    <button onClick$={onClick$} class={[styles.button, styles[variant]]}>
       <Slot />
     </button>
   );
