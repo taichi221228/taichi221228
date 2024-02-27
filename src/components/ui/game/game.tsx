700e61b3b7e3caf7fb28bd7eb331ec2959e47ef4
taichi221228 <taichi221228@icloud.com>
2024-02-27 15:33:54 +0900

    feat: Update UI with directional key components
    Updated the game UI with grid layout to arrange four directional key components, each representing up, down, left, and right respectively. CSS styles and margin were added to properly space the keys. The keys are now functional and the layout contributes to a more intuitive user interaction.

--- src/components/ui/game/game.tsx
+++ src/components/ui/game/game.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 
 import styles from "./game.module.css";
+import { Key } from "./key";
 import { Polygon } from "./polygon";
 import { Button } from "../button/button";
 
@@ -21,7 +22,14 @@ export const Game = component$(() => {
         <div class={styles.help}>
           <p>use keyboard</p>
           <p>arrows to play</p>
-          <div class={styles.keys}></div>
+          <div class={styles.keys}>
+            <div />
+            <Key direction="up" />
+            <div />
+            <Key direction="left" />
+            <Key direction="down" />
+            <Key direction="right" />
+          </div>
         </div>
         <div class={styles.score}></div>
       </div>
