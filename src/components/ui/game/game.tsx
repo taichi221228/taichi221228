268e8eb32f20ca95adca496c40410ad4ffaa13d2
taichi221228 <taichi221228@icloud.com>
2024-02-27 14:58:03 +0900

    feat: Add 'start-game' button to game UI
    A 'start-game' button has been added into the main area of the game UI using absolute position. This change involves modifications both in 'game.module.css' and 'game.tsx' files. The button appears in the middle-left area of the screen, which makes it easy for user interaction.

--- src/components/ui/game/game.tsx
+++ src/components/ui/game/game.tsx
@@ -2,11 +2,16 @@ import { component$ } from "@builder.io/qwik";
 
 import styles from "./game.module.css";
 import { Polygon } from "./polygon";
+import { Button } from "../button/button";
 
 export const Game = component$(() => {
   return (
     <div class={styles.container}>
-      <div class={styles.main}></div>
+      <div class={styles.main}>
+        <Button onClick$={() => {}} variant="accent">
+          start-game
+        </Button>
+      </div>
 
       <div class={styles.sub}></div>
 
