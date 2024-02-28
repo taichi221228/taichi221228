21a1ccc5f5cdcc89267b9359ec2ca19829be4edf
taichi221228 <taichi221228@icloud.com>
2024-02-28 13:11:37 +0900

    refactor: Update 'isEaten' prop type in Food component
    The 'isEaten' prop type in the Food component was mistakenly set as 'true' only. This has been corrected to accept any boolean value to ensure that it can correctly reflect both 'eaten' and 'not eaten' states.

--- src/components/ui/game/food.tsx
+++ src/components/ui/game/food.tsx
@@ -3,9 +3,9 @@ import { component$ } from "@builder.io/qwik";
 import styles from "./food.module.css";
 
 type Props = {
-  isEaten?: true;
+  isEaten?: boolean;
 };
 
-export const Food = component$<Props>(({ isEaten }) => {
+export const Food = component$<Props>(({ isEaten = false }) => {
   return <div class={[styles.food, isEaten && styles.eaten]} />;
 });
