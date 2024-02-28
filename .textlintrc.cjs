88275ac0ece802a0f11ec66da134b63f77f8f9f2
taichi221228 <taichi221228@icloud.com>
2024-02-28 10:20:49 +0900

    chore: Add textlint-rule-alex to textlintrc.cjs file
    This commit includes adding the 'textlint-rule-alex' to the textlintrc.cjs file. This rule helps in checking the written text in markdown files, ensuring language inclusivity and avoiding potentially offensive terms or phrases.

--- .textlintrc.cjs
+++ .textlintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "textlint-rule-no-zero-width-spaces": true,
     "textlint-rule-doubled-spaces": true,
     "@textlint-rule/textlint-rule-no-duplicate-abbr": true,
+    "textlint-rule-alex": true,
     "textlint-rule-apostrophe": true,
     "textlint-rule-diacritics": true,
     "textlint-rule-en-capitalization": true,
