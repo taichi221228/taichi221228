ce50cca2d7d0519504e4fe2a390dfba5ee09497b
taichi221228 <taichi221228@icloud.com>
2024-03-14 22:38:24 +0900

    feat: Add empty wrapper style to layout module
    A new CSS module has been created for the Layout component to contain an empty wrapper style, called layout.module.css. Moreover, the previous `<main>` HTML tag has been now surrounded by a div using this new wrapper class, while still retaining its contents unchanged. This wrapper style enforces a consistent way of managing the height of the main body content across different components and screen sizes.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,15 +4,19 @@ import { Footer } from "~/components/ui/footer/footer";
 import { Header } from "~/components/ui/header/header";
 import { useGlobalStyles } from "~/hooks/global-styles/global-styles";
 
+import styles from "./layout.module.css";
+
 export default component$(() => {
   useGlobalStyles();
 
   return (
     <>
       <Header />
-      <main>
-        <Slot />
-      </main>
+      <div class={styles.wrapper}>
+        <main>
+          <Slot />
+        </main>
+      </div>
       <Footer />
     </>
   );
