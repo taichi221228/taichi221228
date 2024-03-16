7a30d6a1ea959cd5c096eb59e5edcad08ca13e12
taichi221228 <taichi221228@icloud.com>
2024-03-16 17:28:29 +0900

    fix: Replace TITLE constant with SITENAME in header component
    In the header component, the previous TITLE constant has been replaced with the more aptly named SITENAME constant.

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,7 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import { useLocation } from "@builder.io/qwik-city";
 
-import { TITLE } from "~/constants/info";
+import { SITENAME } from "~/constants/info";
 
 import styles from "./header.module.css";
 import { Navigation } from "./navigation";
@@ -12,7 +12,9 @@ export const Header = component$(() => {
 
 	return (
 		<header class={styles.header}>
-			<h1 class={styles.title}>{isRoot ? TITLE : <a href="/">{TITLE}</a>}</h1>
+			<h1 class={styles.title}>
+				{isRoot ? SITENAME : <a href="/">{SITENAME}</a>}
+			</h1>
 			<Navigation location={location} />
 		</header>
 	);
