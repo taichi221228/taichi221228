f168c7e28476463b63a311797226a09e5371233d
taichi221228 <taichi221228@icloud.com>
2024-03-16 22:57:16 +0900

    feat: Refactor CSS and HTML for main-visual component
    This commit revises the layout for the main-visual component by changing the container types and adjusting margins based on the viewport. The section tag is also changed to match the new layout structure. This refactoring enhances the responsiveness and improves the overall display on different screen sizes.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,43 +9,41 @@ import styles from "./index.module.css";
 
 export default component$(() => {
 	return (
-		<section>
-			<div class={styles.wrapper}>
-				<div class={styles.container}>
-					<div class={styles.title}>
-						<h2>Hi all. I am</h2>
-						<h1>{NAME}</h1>
-						<h3>&gt; Front-end developer</h3>
-					</div>
-					<div class={styles.game}>
-						<Game />
-					</div>
-					<div class={styles.description}>
-						<p class={styles.comment}>
-							<span class={styles.line}>{"// "}</span>
-							<span class={styles.block}>{"/* "}</span>
-							complete the game to continue
-							<span class={styles.block}>{" */"}</span>
-						</p>
-						<p class={styles.comment}>
-							<span class={styles.line}>{"// "}</span>
-							<span class={styles.block}>{"/* "}</span>
-							you can also see it on my Github page
-							<span class={styles.block}>{" */"}</span>
-						</p>
-						<p class={styles.declaration}>
-							<span class={styles.kind}>const</span>{" "}
-							<span class={styles.identifier}>githubLink</span> = <br />
-							<span class={styles.literal}>
-								{'"'}
-								<a href={SNS.github.url} target="_blank" rel="noreferrer">
-									{SNS.github.url}
-								</a>
-								{'"'}
-							</span>
-							;
-						</p>
-					</div>
+		<section class={styles.wrapper}>
+			<div class={styles.container}>
+				<div class={styles.title}>
+					<h2>Hi all. I am</h2>
+					<h1>{NAME}</h1>
+					<h3>&gt; Front-end developer</h3>
+				</div>
+				<div class={styles.game}>
+					<Game />
+				</div>
+				<div class={styles.description}>
+					<p class={styles.comment}>
+						<span class={styles.line}>{"// "}</span>
+						<span class={styles.block}>{"/* "}</span>
+						complete the game to continue
+						<span class={styles.block}>{" */"}</span>
+					</p>
+					<p class={styles.comment}>
+						<span class={styles.line}>{"// "}</span>
+						<span class={styles.block}>{"/* "}</span>
+						you can also see it on my Github page
+						<span class={styles.block}>{" */"}</span>
+					</p>
+					<p class={styles.declaration}>
+						<span class={styles.kind}>const</span>{" "}
+						<span class={styles.identifier}>githubLink</span> = <br />
+						<span class={styles.literal}>
+							{'"'}
+							<a href={SNS.github.url} target="_blank" rel="noreferrer">
+								{SNS.github.url}
+							</a>
+							{'"'}
+						</span>
+						;
+					</p>
 				</div>
 			</div>
 		</section>
