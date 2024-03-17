dae115469ad089a600e6b50c3b097fffa78f045a
taichi221228 <taichi221228@icloud.com>
2024-03-17 13:27:14 +0900

    refactor: Remove layout.module.css file
    The `layout.module.css` file was deleted and its contents (block-size and overflow properties) were moved to the `global.css` file. Further, the usage of styles from this file was eliminated in the `layout.tsx` file. These changes simplify the overall code structure and centralize the style definitions.

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,19 +4,15 @@ import { Footer } from "~/components/ui/footer/footer";
 import { Header } from "~/components/ui/header/header";
 import { useGlobalStyles } from "~/hooks/global-styles/global-styles";
 
-import styles from "./layout.module.css";
-
 export default component$(() => {
 	useGlobalStyles();
 
 	return (
 		<>
 			<Header />
-			<div class={styles.wrapper}>
-				<main>
-					<Slot />
-				</main>
-			</div>
+			<main>
+				<Slot />
+			</main>
 			<Footer />
 		</>
 	);
