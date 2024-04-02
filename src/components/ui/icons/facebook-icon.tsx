cb8d167eca35d13c7d2c9aa0738f1b697b4a59e2
taichi221228 <taichi221228@icloud.com>
2024-04-02 21:28:16 +0900

    feat: Add Facebook, GitHub, and Twitter icons
    Three new icon components have been created and exported from the ui/icons directory: FacebookIcon, GitHubIcon, and TwitterIcon.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

---
+++ src/components/ui/icons/facebook-icon.tsx
@@ -0,0 +1,5 @@
+import { component$ } from "@builder.io/qwik";
+
+export const FacebookIcon = component$(() => {
+	return <i class="ri-facebook-fill" />;
+});
---
+++ src/components/ui/icons/twitter-icon.tsx
@@ -0,0 +1,5 @@
+import { component$ } from "@builder.io/qwik";
+
+export const TwitterIcon = component$(() => {
+	return <i class="ri-twitter-fill" />;
+});
