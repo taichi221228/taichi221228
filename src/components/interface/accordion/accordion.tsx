0415b078d69af9eef5c52b008acdc006015f123d
taichi221228 <taichi221228@icloud.com>
2024-04-07 17:22:01 +0900

    feat: Add optional `shouldOpen` prop to Accordion component
    Added an optional `shouldOpen` prop to the Accordion component, which determines if the accordion should open initially.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/components/interface/accordion/accordion.tsx
+++ src/components/interface/accordion/accordion.tsx
@@ -7,28 +7,31 @@ import styles from "./accordion.module.css";
 
 type Props = {
 	label: string;
+	shouldOpen?: boolean;
 } & PolyProps<"div" | "nav">;
 
-export const Accordion = component$<Props>(({ as, label }) => {
-	const isOpen = useSignal(false);
+export const Accordion = component$<Props>(
+	({ as, label, shouldOpen = false }) => {
+		const isOpen = useSignal(shouldOpen);
 
-	return (
-		<Poly class={styles.accordion} as={as}>
-			<button
-				class={styles.header}
-				onClick$={() => {
-					isOpen.value = !isOpen.value;
-				}}
-				type="button"
-			>
-				<Triangle direction={isOpen.value ? "up" : "down"} />
-				{label}
-			</button>
-			<div class={[styles.content, isOpen.value && styles.opened]}>
-				<div class={styles.container}>
-					<Slot />
+		return (
+			<Poly class={styles.accordion} as={as}>
+				<button
+					class={styles.header}
+					onClick$={() => {
+						isOpen.value = !isOpen.value;
+					}}
+					type="button"
+				>
+					<Triangle direction={isOpen.value ? "up" : "down"} />
+					{label}
+				</button>
+				<div class={[styles.content, isOpen.value && styles.opened]}>
+					<div class={styles.container}>
+						<Slot />
+					</div>
 				</div>
-			</div>
-		</Poly>
-	);
-});
+			</Poly>
+		);
+	},
+);
