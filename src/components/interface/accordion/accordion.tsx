63e1b4a89c349b9fc5b2775eb5a3be083b284867
taichi221228 <taichi221228@icloud.com>
2024-04-09 22:20:58 +0900

    fix: Accordion component to use slots
    The 'label' prop has been removed in the Accordion component and replaced with slots. This change was made to resolve an issue where using Signal in an accordion header would not trigger a re-render correctly.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/components/interface/accordion/accordion.tsx
+++ src/components/interface/accordion/accordion.tsx
@@ -6,32 +6,29 @@ import { Triangle } from "~/components/interface/triangle/triangle";
 import styles from "./accordion.module.css";
 
 type Props = {
-	label: string;
 	shouldOpen?: boolean;
 } & PolyProps<"div" | "nav">;
 
-export const Accordion = component$<Props>(
-	({ as, label, shouldOpen = false }) => {
-		const isOpen = useSignal(shouldOpen);
+export const Accordion = component$<Props>(({ as, shouldOpen = false }) => {
+	const isOpen = useSignal(shouldOpen);
 
-		return (
-			<Poly class={styles.accordion} as={as}>
-				<button
-					class={styles.header}
-					onClick$={() => {
-						isOpen.value = !isOpen.value;
-					}}
-					type="button"
-				>
-					<Triangle direction={isOpen.value ? "down" : "up"} />
-					{label}
-				</button>
-				<div class={[styles.content, isOpen.value && styles.opened]}>
-					<div class={styles.container}>
-						<Slot />
-					</div>
+	return (
+		<Poly class={styles.accordion} as={as}>
+			<button
+				class={styles.header}
+				onClick$={() => {
+					isOpen.value = !isOpen.value;
+				}}
+				type="button"
+			>
+				<Triangle direction={isOpen.value ? "down" : "up"} />
+				<Slot name="header" />
+			</button>
+			<div class={[styles.content, isOpen.value && styles.opened]}>
+				<div class={styles.container}>
+					<Slot name="content" />
 				</div>
-			</Poly>
-		);
-	},
-);
+			</div>
+		</Poly>
+	);
+});
