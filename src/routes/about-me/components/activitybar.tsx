0ccc76aec69fde2daac4523e6f3c6e01f2ef70b7
taichi221228 <taichi221228@icloud.com>
2024-04-10 23:00:56 +0900

    refactor: Refactor current property type in about-me components
    The type definition for the 'current' property in the 'Sidebar' and 'Activitybar' components has been improved. A 'Current' type is now defined and exported from the '../index' module to increase code clarity and maintainability.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/routes/about-me/components/activitybar.tsx
+++ src/routes/about-me/components/activitybar.tsx
@@ -1,12 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 
 import styles from "./activitybar.module.css";
-import { activities } from "../index";
+import { activities, type Current } from "../index";
 
 type Props = {
-	current: {
-		activity: (typeof activities)[number]["name"];
-	};
+	current: Current;
 };
 
 export const Activitybar = component$<Props>(({ current }) => {
