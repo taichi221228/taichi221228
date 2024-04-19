0409ee5a0b5fc507606df5722925e9603161d6b7
taichi221228 <taichi221228@icloud.com>
2024-04-19 17:17:16 +0900

    chore: Add "subject-case" rule to commitlint.config.js
    This commit introduces a new "subject-case" rule to our commitlint configuration file. This rule enforces the usage of sentence-case for all commit message subjects, improving consistency across the project.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@
 export default {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
+		"subject-case": [2, "always", "sentence-case"],
 		"body-max-line-length": [2, "always", Number.POSITIVE_INFINITY],
 	},
 };
