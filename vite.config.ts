1bd9596638e21a2c9a3887817aa680269eb64ccf
taichi221228 <taichi221228@icloud.com>
2024-04-24 16:44:50 +0900

    refactor: Update vite config and refactor routes
    Expanded the qwikCity() plugin configuration in the vite.config.ts file to include a rewriteRoutes option. Also renamed and moved the index.module.css and index.tsx files to a new directory, 'hello', under src/routes.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig((): UserConfig => {
 	return {
-		plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+		plugins: [
+			qwikCity({
+				rewriteRoutes: [{ paths: { hello: "" } }],
+			}),
+			qwikVite(),
+			tsconfigPaths(),
+		],
 		server: {
 			headers: {
 				"Cache-Control": "public, max-age=0",
