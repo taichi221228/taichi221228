a2947a0233b51962967bddc0d671f0a4c32b44cc
taichi221228 <taichi221228@icloud.com>
2024-04-29 10:35:04 +0900

    feat: Add items prop to Tabs component
    Added an 'items' prop to the Tabs component in order to render a list. This commit makes the Tabs component more functional by allowing it to display a list of string items, each represented as an individual tab.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/components/interface/tabs.tsx
+++ src/components/interface/tabs.tsx
@@ -2,6 +2,16 @@ import { component$ } from "@builder.io/qwik";
 
 import styles from "./tabs.module.css";
 
-export const Tabs = component$(() => {
-	return <ul class={styles.tabs} />;
+type Props = {
+	items: string[];
+};
+
+export const Tabs = component$<Props>(({ items }) => {
+	return (
+		<ul class={styles.tabs}>
+			{items.map((item) => {
+				return <li key={item}>{item}</li>;
+			})}
+		</ul>
+	);
 });
