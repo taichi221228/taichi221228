1024e634ff9173dae929ddcc78f9fef9b3de7436
taichi221228 <taichi221228@icloud.com>
2024-04-30 10:01:17 +0900

    refactor: Add context provider to 'about-me' route
    The changes include adding context provider to the 'about-me' route. This involved adding a new constant and using the 'useContextProvider' hook. Some imports were also added and updated to accommodate these changes.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/routes/about-me/index.tsx
+++ src/routes/about-me/index.tsx
@@ -1,8 +1,8 @@
-import { type Component, component$, useStore } from "@builder.io/qwik";
+import { type Component, component$, createContextId, useContextProvider, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import { GamepadIcon, TerminalIcon, UserIcon } from "~/components/interface/icons";
-import { NAME } from "~/constants/info";
+import { NAME, SITENAME } from "~/constants/info";
 import { createPageTitle } from "~/utilities/create-page-title";
 
 import { Activitybar } from "./activitybar";
@@ -35,12 +35,17 @@ export type Current = {
 	side: (typeof activities)[number]["contents"][number];
 };
 
+/** @package */
+export const CURRENT = createContextId<Current>([SITENAME, "about-me", "current"].join("."));
+
 /** @private */
 export default component$(() => {
 	const current = useStore<Current>({
 		activity: "personal-info",
 		side: "bio",
 	});
+
+	useContextProvider(CURRENT, current);
 
 	return (
 		<div class={styles.container}>
