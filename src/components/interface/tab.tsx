7642dd3e6480630590133320a3c8c61e3ea11b60
taichi221228 <taichi221228@icloud.com>
2024-05-01 14:33:04 +0900

    feat: Add condition to render tab items
    This update enhances the Tab component code by adding a condition that checks for the presence of 'item' before rendering the list element. This prevents empty tab items from being rendered in the user interface.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/components/interface/tab.tsx
+++ src/components/interface/tab.tsx
@@ -17,12 +17,14 @@ type Props =
 export const Tab = component$<Props>(({ item, onClick$ }) => {
 	return (
 		<ul class={styles.tab}>
-			<li>
-				{item}
-				<button class={styles.close} onClick$={onClick$} type="button">
-					<CloseIcon class={styles.icon} />
-				</button>
-			</li>
+			{item && (
+				<li>
+					{item}
+					<button class={styles.close} onClick$={onClick$} type="button">
+						<CloseIcon class={styles.icon} />
+					</button>
+				</li>
+			)}
 		</ul>
 	);
 });
