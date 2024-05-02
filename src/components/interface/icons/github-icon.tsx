a71c0d5220e47b923d5bb9f08de2b4d679c92ca5
taichi221228 <taichi221228@icloud.com>
2024-05-02 10:12:35 +0900

    refactor: Rename IconProps to Props in github-icon.tsx
    The reference to IconProps in the GitHubIcon component definition was converted to Props for brevity and readability. This change influences only the internal component signature and does not affect component's external API or behavior.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/components/interface/icons/github-icon.tsx
+++ src/components/interface/icons/github-icon.tsx
@@ -1,8 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 
-import { Icon, type IconProps } from "./icon";
+import { Icon, type IconProps as Props } from "./icon";
 
 /** @package */
-export const GitHubIcon = component$<IconProps>(({ ...props }) => {
+export const GitHubIcon = component$<Props>(({ ...props }) => {
 	return <Icon type="ri-github-fill" {...props} />;
 });
