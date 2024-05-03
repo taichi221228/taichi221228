25644adb10a54b18cf1a5fc415d250afe8958db8
taichi221228 <taichi221228@icloud.com>
2024-05-03 11:22:26 +0900

    feat: Implement useSecretLog function
    The useSecretLog function has been enhanced to use the useVisibleTask$ functionality from "@builder.io/qwik". This function now retrieves content and logs it, incorporating enhanced style formatting. The eslint rule for 'no-use-visible-task' has been disabled to allow its use.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/hooks/secret-log.ts
+++ src/hooks/secret-log.ts
@@ -1,1 +1,15 @@
-export const useSecretLog = () => {};
+import { useVisibleTask$ } from "@builder.io/qwik";
+
+import { contents } from "./secret-log/contents";
+
+export const useSecretLog = () => {
+	// eslint-disable-next-line qwik/no-use-visible-task
+	useVisibleTask$(() => {
+		const [content] = contents;
+		const { art, text, styles } = content;
+
+		[art, text].map((body) => {
+			console.log(`%c${body}`, styles.join(" "));
+		});
+	});
+};
