e00f30bb94c012d689702ccac49e0cc0442a838b
taichi221228 <taichi221228@icloud.com>
2024-05-03 09:27:26 +0900

    refactor: Simplify global-styles file path
    The global-styles file has been moved up one directory level, removing an unnecessary sub-directory. This revision improves file organization and path simplicity.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot } from "@builder.io/qwik";
 
 import { Footer } from "~/components/interface/footer";
 import { Header } from "~/components/interface/header";
-import { useGlobalStyles } from "~/hooks/global-styles/global-styles";
+import { useGlobalStyles } from "~/hooks/global-styles";
 
 /** @private */
 export default component$(() => {
