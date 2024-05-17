cdf841c7adb68cb6fffedfbf77a55bf74b18bc8b
taichi221228 <taichi221228@icloud.com>
2024-05-17 16:23:26 +0900

    refactor: Move global.css import to global-styles.ts
    The import statement for global.css was removed from root.tsx and moved to global-styles.ts. This change better organizes the project's structure.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/hooks/global-styles.ts
+++ src/hooks/global-styles.ts
@@ -4,6 +4,8 @@ import fontStyles from "@fontsource/fira-code?inline";
 import resetStyles from "@unocss/reset/tailwind.css?inline";
 import iconStyles from "remixicon/fonts/remixicon.css?inline";
 
+import "~/global.css";
+
 export const useGlobalStyles = () => {
 	useStyles$(resetStyles);
 	useStyles$(fontStyles);
