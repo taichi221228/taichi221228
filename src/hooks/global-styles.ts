c6d6d3aca08515060bee3dd762d5bd40f5eaf09e
taichi221228 <taichi221228@icloud.com>
2024-05-18 10:49:19 +0900

    feat: Add Gruvbox theme to global styles
    A new import for the Gruvbox theme has been added to the `global-styles.ts` file. This change allows the theme to be globally available across the application.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/hooks/global-styles.ts
+++ src/hooks/global-styles.ts
@@ -5,6 +5,7 @@ import reset from "@unocss/reset/tailwind.css?inline";
 import icon from "remixicon/fonts/remixicon.css?inline";
 
 import "~/global.css";
+import "~/styles/theme/gruvbox.css";
 
 export const useGlobalStyles = () => {
 	useStyles$(reset);
