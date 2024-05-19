a5699c986350aadf8b5c0d64fb48640a75248368
taichi221228 <taichi221228@icloud.com>
2024-05-19 10:10:46 +0900

    feat: Added conditional styling for root page header and footer
    The header and footer styles have been modified to conditionally apply a specific style when on the root ("/") path of the website. This is implemented as a temporary solution for styling and is indicated as needing further refactoring to improve the method of applying these conditional styles.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/components/interface/footer.tsx
+++ src/components/interface/footer.tsx
@@ -1,13 +1,16 @@
-import {component$} from "@builder.io/qwik";
-import {useLocation} from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 
-import {type Sns, SNSS, type Snss} from "~/constants/info";
+import { type Sns, SNSS, type Snss } from "~/constants/info";
 
 import styles from "./footer.module.css";
 
 export const Footer = component$(() => {
+	const location = useLocation();
+	const isRoot = "/" === location.url.pathname;
+
 	return (
-		<footer class={styles.footer}>
+		<footer class={[styles.footer, /* TODO: Not a smart way. Need refactoring */ isRoot && styles.root]}>
 			<p>find me in:</p>
 			{((["facebook", "x", "github"] satisfies (keyof Snss)[]).map((name) => [name, SNSS[name]]) satisfies [keyof Snss, Sns][]).map(
 				([name, { Logo, ...profile }]) => {
