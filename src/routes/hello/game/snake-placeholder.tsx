3dfc2fc722f1f41ec831002c7fb70d772600cbfb
taichi221228 <taichi221228@icloud.com>
2024-05-21 10:37:39 +0900

    refactor: Refactor color styling in snake game placeholder
    The styling configuration for the color of the circle elements and linear gradients in the Snake Game placeholder has been moved from the .tsx file to the CSS module. This improves separation of concerns between design and logic.
    
    Signed-off-by: taichi221228 <taichi221228@icloud.com>

--- src/routes/hello/game/snake-placeholder.tsx
+++ src/routes/hello/game/snake-placeholder.tsx
@@ -8,13 +8,13 @@ export const SnakePlaceholder = component$(() => {
 		<div class={styles.container}>
 			<svg role="img" aria-label='A placeholder for the "Snake Game" component.' width="67" height="197" viewBox="0 0 67 197" fill="none">
 				<path d="M10.8271 39.0794V114.079H62.8271V192.691" stroke="url(#paint0_linear_0_1)" stroke-width="8" stroke-linecap="round" />
-				<circle opacity="0.1" cx="10.827" cy="10.9636" r="10.3456" fill="var(--color-accent-4)" />
-				<circle opacity="0.2" cx="10.827" cy="10.9636" r="7.34558" fill="var(--color-accent-4)" />
-				<circle cx="10.8271" cy="10.9636" r="4" fill="var(--color-accent-4)" />
+				<circle opacity="0.1" cx="10.827" cy="10.9636" r="10.3456" />
+				<circle opacity="0.2" cx="10.827" cy="10.9636" r="7.34558" />
+				<circle cx="10.8271" cy="10.9636" r="4" />
 				<defs>
 					<linearGradient id="paint0_linear_0_1" x1="10.8271" y1="48.0794" x2="73.9999" y2="166.691" gradientUnits="userSpaceOnUse">
-						<stop stop-color="var(--color-accent-4)" />
-						<stop offset="1" stop-color="var(--color-accent-4)" stop-opacity="0" />
+						<stop />
+						<stop offset="1" stop-opacity="0" />
 					</linearGradient>
 				</defs>
 			</svg>
