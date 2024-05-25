ebb9f73aaddd64c066439cda245164962180dcf9
taichi221228 <taichi221228@icloud.com>
2024-05-25 11:22:25 +0900

    feat: Add sidebar to contact page
    A sidebar was integrated into the contact page to enhance navigation or functionality. Relevant CSS styles were updated to display the contact page as a flex container and style the new sidebar properly.

--- src/routes/contact-me/index.tsx
+++ src/routes/contact-me/index.tsx
@@ -1,11 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
 import styles from "./index.module.css";
+import { Sidebar } from "./sidebar";
 
 /** @private */
 export default component$(() => {
 	return (
 		<div class={styles.container}>
+			<aside>
+				<Sidebar />
+			</aside>
 			<p>Hello, scaffdog!</p>
 		</div>
 	);
