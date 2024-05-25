afb1bee2889d42f6c6db9e7774e123ac3a94d180
taichi221228 <taichi221228@icloud.com>
2024-05-25 16:10:36 +0900

    feat: Update contact sidebar with accordion and icons
    Enhanced the contact sidebar on the 'contact-me' route by replacing plain text with an accordion containing email and phone info. Added mail and phone icons for more intuitive interaction. CSS styles for the sidebar piece have also been updated in sidebar.module.css.

--- src/routes/contact-me/sidebar.tsx
+++ src/routes/contact-me/sidebar.tsx
@@ -1,12 +1,31 @@
 import { component$ } from "@builder.io/qwik";
 
+import { Accordion } from "~/components/interface/accordion";
+import { MailIcon, PhoneIcon } from "~/components/interface/icons";
+import { EMAIL, PHONE, USERNAME } from "~/constants/info";
+
 import styles from "./sidebar.module.css";
 
 /** @package */
 export const Sidebar = component$(() => {
 	return (
-		<div class={styles.container}>
-			<p>Hello, scaffdog!</p>
+		<div class={styles.sidebar}>
+			<Accordion head="contacts" shouldOpen={true}>
+				<ul>
+					<li>
+						<a href={`mailto:${EMAIL}`}>
+							<MailIcon />
+							{USERNAME}
+						</a>
+					</li>
+					<li>
+						<a href={`tel:${PHONE}`}>
+							<PhoneIcon />
+							{PHONE}
+						</a>
+					</li>
+				</ul>
+			</Accordion>
 		</div>
 	);
 });
