1a2dd7e90445599d2b8ec2fc66059ec6bdcf81ac
taichi221228 <taichi221228@icloud.com>
2024-05-28 11:18:10 +0900

    feat: Update ContactsAccordion component with new styles and content
    In this commit, the ContactsAccordion component was updated both in style and content. CSS styling was applied to create a more pleasing interface, specifying variables for padding, color and border-radius. For content, added a list of contact options - email and phone, wrapping them in Accordion component. Also, 'MailIcon' and 'PhoneIcon' were added to visually represent contact options.

--- src/components/interface/accordions/contacts-accordion.tsx
+++ src/components/interface/accordions/contacts-accordion.tsx
@@ -1,12 +1,29 @@
 import { component$ } from "@builder.io/qwik";
 
+import { Accordion } from "~/components/interface/accordions/accordion";
+import { MailIcon, PhoneIcon } from "~/components/interface/icons";
+import { EMAIL, PHONE, USERNAME } from "~/constants/info";
+
 import styles from "./contacts-accordion.module.css";
 
 /** @package */
 export const ContactsAccordion = component$(() => {
 	return (
-		<div class={styles.container}>
-			<h1>Hello, scaffdog!</h1>
-		</div>
+		<Accordion head="contacts" shouldOpen={true}>
+			<ul class={styles.body}>
+				<li>
+					<a href={`mailto:${EMAIL}`}>
+						<MailIcon />
+						{USERNAME}
+					</a>
+				</li>
+				<li>
+					<a href={`tel:${PHONE}`}>
+						<PhoneIcon />
+						{PHONE}
+					</a>
+				</li>
+			</ul>
+		</Accordion>
 	);
 });
