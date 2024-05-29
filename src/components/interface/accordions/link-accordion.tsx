d88b5e492ac14a3552669721b7435256ac53431e
taichi221228 <taichi221228@icloud.com>
2024-05-30 08:53:23 +0900

    feat: Update `<LinkAccordion />` and styles
    The LinkAccordion component now leverages the Accordion component and allows for custom prop configurations. This includes defining 'head' and 'shouldOpen' properties. Additionally, we've updated the link-accordion.module.css to define a .body class that includes specifications for list items. This refactoring not only enhances performance but also improves maintainability of the code.

--- src/components/interface/accordions/link-accordion.tsx
+++ src/components/interface/accordions/link-accordion.tsx
@@ -1,12 +1,17 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, Slot } from "@builder.io/qwik";
 
+import { Accordion, type AccordionProps } from "./accordion";
 import styles from "./link-accordion.module.css";
 
+type Props = Pick<AccordionProps, "head" | "shouldOpen">;
+
 /** @package */
-export const LinkAccordion = component$(() => {
+export const LinkAccordion = component$(({ head, shouldOpen }: Props) => {
 	return (
-		<div class={styles.container}>
-			<h1>Hello, scaffdog!</h1>
-		</div>
+		<Accordion head={head} shouldOpen={shouldOpen}>
+			<ul class={styles.body}>
+				<Slot />
+			</ul>
+		</Accordion>
 	);
 });
