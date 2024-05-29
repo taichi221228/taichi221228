b6604cbee2890109e5e42d6eb535c72f3e2123a6
taichi221228 <taichi221228@icloud.com>
2024-05-30 08:58:08 +0900

    refactor: Refactor sidebar to use `<LinkAccordion />`
    The sidebar component has been refactored to use the new LinkAccordion component, which replaces the previous Accordion component. This simplifies the sidebar's structure and makes the codebase more consistent. Additionally, specific styling related to the accordion within the sidebar CSS module has been removed, as it is now redundant.

--- src/routes/contact-me/sidebar.tsx
+++ src/routes/contact-me/sidebar.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 
-import { Accordion, ContactsAccordion } from "~/components/interface/accordions";
+import { ContactsAccordion, LinkAccordion } from "~/components/interface/accordions";
 import { ExternalLinkIcon } from "~/components/interface/icons";
 
 import styles from "./sidebar.module.css";
@@ -10,34 +10,32 @@ export const Sidebar = component$(() => {
 	return (
 		<div class={styles.sidebar}>
 			<ContactsAccordion />
-			<Accordion head="find-me-also-in" shouldOpen={true}>
-				<ul>
-					<li>
-						<a rel="noreferrer" href="https://youtube.com" target="_blank">
-							<ExternalLinkIcon />
-							YouTube channel
-						</a>
-					</li>
-					<li>
-						<a rel="noreferrer" href="https://gurushots.com" target="_blank">
-							<ExternalLinkIcon />
-							GuruShots profile
-						</a>
-					</li>
-					<li>
-						<a rel="noreferrer" href="https://instagram.com" target="_blank">
-							<ExternalLinkIcon />
-							Instagram account
-						</a>
-					</li>
-					<li>
-						<a rel="noreferrer" href="https://twitch.tv" target="_blank">
-							<ExternalLinkIcon />
-							Twitch profile
-						</a>
-					</li>
-				</ul>
-			</Accordion>
+			<LinkAccordion head="find-me-also-in" shouldOpen={true}>
+				<li>
+					<a rel="noreferrer" href="https://youtube.com" target="_blank">
+						<ExternalLinkIcon />
+						YouTube channel
+					</a>
+				</li>
+				<li>
+					<a rel="noreferrer" href="https://gurushots.com" target="_blank">
+						<ExternalLinkIcon />
+						GuruShots profile
+					</a>
+				</li>
+				<li>
+					<a rel="noreferrer" href="https://instagram.com" target="_blank">
+						<ExternalLinkIcon />
+						Instagram account
+					</a>
+				</li>
+				<li>
+					<a rel="noreferrer" href="https://twitch.tv" target="_blank">
+						<ExternalLinkIcon />
+						Twitch profile
+					</a>
+				</li>
+			</LinkAccordion>
 		</div>
 	);
 });
