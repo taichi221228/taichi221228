8c05e1d5d72c53ca2dcd6f407538f30fc3977898
taichi221228 <taichi221228@icloud.com>
2024-05-30 08:52:41 +0900

    feat: Update `<ContactsAccordion />` to use dynamic 'shouldOpen' prop
    The ContactsAccordion component is modified to use the 'shouldOpen' prop passed down from its parent components instead of manually setting to true. This provides more flexibility by allowing parent components to decide if the accordion should be open by default.

--- src/components/interface/accordions/contacts-accordion.tsx
+++ src/components/interface/accordions/contacts-accordion.tsx
@@ -4,12 +4,15 @@ import { Accordion } from "~/components/interface/accordions/accordion";
 import { MailIcon, PhoneIcon } from "~/components/interface/icons";
 import { EMAIL, PHONE, USERNAME } from "~/constants/info";
 
+import type { AccordionProps } from "./accordion";
 import styles from "./contacts-accordion.module.css";
 
+type Props = Pick<AccordionProps, "shouldOpen">;
+
 /** @package */
-export const ContactsAccordion = component$(() => {
+export const ContactsAccordion = component$(({ shouldOpen }: Props) => {
 	return (
-		<Accordion head="contacts" shouldOpen={true}>
+		<Accordion head="contacts" shouldOpen={shouldOpen}>
 			<ul class={styles.body}>
 				<li>
 					<a href={`mailto:${EMAIL}`}>
