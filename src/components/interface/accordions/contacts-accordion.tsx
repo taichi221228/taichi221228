30631d58109fd5fda936f70393dd2dac17a73242
taichi221228 <taichi221228@icloud.com>
2024-05-30 08:55:00 +0900

    feat: Replace `<ContactsAccordion />` with `<LinkAccordion />`
    The ContactsAccordion has been replaced with the LinkAccordion component, removing the need for the dedicated contacts-accordion.module.css file, which has been deleted. The changes should simplify the component structure and remove unnecessary CSS code.

--- src/components/interface/accordions/contacts-accordion.tsx
+++ src/components/interface/accordions/contacts-accordion.tsx
@@ -1,32 +1,29 @@
 import { component$ } from "@builder.io/qwik";
 
-import { Accordion } from "~/components/interface/accordions/accordion";
 import { MailIcon, PhoneIcon } from "~/components/interface/icons";
 import { EMAIL, PHONE, USERNAME } from "~/constants/info";
 
 import type { AccordionProps } from "./accordion";
-import styles from "./contacts-accordion.module.css";
+import { LinkAccordion } from "./link-accordion";
 
 type Props = Pick<AccordionProps, "shouldOpen">;
 
 /** @package */
 export const ContactsAccordion = component$(({ shouldOpen }: Props) => {
 	return (
-		<Accordion head="contacts" shouldOpen={shouldOpen}>
-			<ul class={styles.body}>
-				<li>
-					<a href={`mailto:${EMAIL}`}>
-						<MailIcon />
-						{USERNAME}
-					</a>
-				</li>
-				<li>
-					<a href={`tel:${PHONE}`}>
-						<PhoneIcon />
-						{PHONE}
-					</a>
-				</li>
-			</ul>
-		</Accordion>
+		<LinkAccordion head="contacts" shouldOpen={shouldOpen}>
+			<li>
+				<a href={`mailto:${EMAIL}`}>
+					<MailIcon />
+					{USERNAME}
+				</a>
+			</li>
+			<li>
+				<a href={`tel:${PHONE}`}>
+					<PhoneIcon />
+					{PHONE}
+				</a>
+			</li>
+		</LinkAccordion>
 	);
 });
