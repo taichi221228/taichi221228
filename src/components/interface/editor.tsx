a3cbd60586900a7bcc4432493df5119a2697e5c6
taichi221228 <taichi221228@icloud.com>
2024-06-01 14:23:50 +0900

    refactor: Removed Pane component and integrated its styling into Editor
    The Pane component and its associated .css module were removed from src/components/interface/editor. Instead, the key properties of the Pane module were directly integrated into the Editor module's .css and .tsx files. This simplifies the structure and improves readability by reducing the number of individual component files.

--- src/components/interface/editor.tsx
+++ src/components/interface/editor.tsx
@@ -1,10 +1,8 @@
 import { type Component, component$ } from "@builder.io/qwik";
 
-import { Pane } from "~/components/interface/editor/pane";
+import { Tab, type TabProps } from "~/components/interface/tab";
 
 import styles from "./editor.module.css";
-
-import type { TabProps } from "~/components/interface/tab";
 
 type Props = {
 	panes: [{ tab?: TabProps; Content?: Component }] | [{ tab?: TabProps; Content?: Component }, { tab?: TabProps; Content?: Component }];
@@ -13,11 +11,14 @@ type Props = {
 export const Editor = component$(({ panes }: Props) => {
 	return (
 		<div class={styles.editor}>
-			{panes.map(({ tab, Content = component$(() => <></>) }) => {
+			{panes.map(({ tab, Content = () => <></> }) => {
 				return (
-					<Pane key={tab?.item} tab={{ ...tab }}>
-						<Content />
-					</Pane>
+					<section key={tab?.item} class={styles.pane}>
+						<Tab {...tab} />
+						<div class={styles.content}>
+							<Content />
+						</div>
+					</section>
 				);
 			})}
 		</div>
