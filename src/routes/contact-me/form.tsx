e89d6b585fa0f2a82d6c075c0b0a279975ec7fe6
taichi221228 <taichi221228@icloud.com>
2024-06-02 10:37:40 +0900

    feat: Implement form functionality in contact-me route
    Added useForm functionality to the form in the contact-me route.
    `useForm` is being utilized from "@modular-forms/qwik" to handle form state and operations by defining a scheme for form fields. This change allows form fields such as name, email, and message to hold their respective values, improving form handling and data submission.

--- src/routes/contact-me/form.tsx
+++ src/routes/contact-me/form.tsx
@@ -1,12 +1,34 @@
 import { component$ } from "@builder.io/qwik";
 
+import { useForm } from "@modular-forms/qwik";
+
 import styles from "./form.module.css";
+
+type Scheme = {
+	name: string;
+	email: string;
+	message: string;
+};
 
 /** @package */
 export const Form = component$(() => {
+	const [, { Form, Field }] = useForm<Scheme>({
+		loader: {
+			value: {
+				name: "",
+				email: "",
+				message: "",
+			},
+		},
+	});
+
 	return (
 		<div class={styles.container}>
-			<h1>Hello, scaffdog!</h1>
+			<Form>
+				<Field name="name">{(_, props) => <input {...props} />}</Field>
+				<Field name="email">{(_, props) => <input type="email" {...props} />}</Field>
+				<Field name="message">{(_, props) => <textarea {...props} />}</Field>
+			</Form>
 		</div>
 	);
 });
