5eff9ae9f631e4590f93842df3d366f01bf7d710
taichi221228 <taichi221228@icloud.com>
2024-06-04 22:47:19 +0900

    feat: Add support for submit button type in interface button component
    In the button component of the interface, an enhancement has been made to support both "button" and "submit" types. By default, it's type is set to "button" but now it also accepts "submit" as a valid type. This extends the button's functionality and compatibility with various use-cases.

--- src/components/interface/button.tsx
+++ src/components/interface/button.tsx
@@ -4,12 +4,20 @@ import styles from "./button.module.css";
 
 type Props = {
 	variant?: "primary" | "accent" | "ghost";
-	onClick$: QRL<() => void>;
-};
+} & (
+	| {
+			type?: "button";
+			onClick$: QRL<() => void>;
+	  }
+	| {
+			type: "submit";
+			onClick$?: QRL<() => void>;
+	  }
+);
 
-export const Button = component$(({ variant, onClick$ }: Props) => {
+export const Button = component$(({ variant, type = "button", onClick$ }: Props) => {
 	return (
-		<button class={[styles[variant ?? "primary"], styles.button]} onClick$={onClick$} type="button">
+		<button class={[styles[variant ?? "primary"], styles.button]} onClick$={onClick$} type={type}>
 			<Slot />
 		</button>
 	);
