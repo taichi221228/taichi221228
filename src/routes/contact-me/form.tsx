459472f779395f61bac9f44c24a4bc652e0acbb7
taichi221228 <taichi221228@icloud.com>
2024-06-06 09:28:17 +0900

    refactor: Refactor form field structure in contact-me route
    The code for the form fields in the contact-me route has been refactored for simplicity. The encapsulating div element has been moved inside the function which returns each field, instead of being outside. This reorganization makes the code cleaner and easier to read and maintain.

--- src/routes/contact-me/form.tsx
+++ src/routes/contact-me/form.tsx
@@ -31,47 +31,41 @@ export const Form = component$(() => {
 
 	return (
 		<Form class={styles.form} onSubmit$={handleSubmit$}>
-			<div class={styles.item}>
-				<Field name="name" validate={[required<string>("Please enter you name.")]}>
-					{(store, props) => (
-						<>
-							<label for={store.name}>_{store.name}:</label>
-							<input id={store.name} placeholder={NAME} {...props} required />
-							{store.error && <p>{store.error}</p>}
-						</>
-					)}
-				</Field>
-			</div>
-			<div class={styles.item}>
-				<Field name="email" validate={[required<string>("Please enter your email.")]}>
-					{(store, props) => (
-						<>
-							<label for={store.name}>_{store.name}:</label>
-							<input id={store.name} type="email" placeholder={EMAIL} {...props} required />
-							{store.error && <p>{store.error}</p>}
-						</>
-					)}
-				</Field>
-			</div>
-			<div class={styles.item}>
-				<Field name="message" validate={[required<string>("Please enter your message.")]}>
-					{(store, props) => (
-						<>
-							<label for={store.name}>_{store.name}:</label>
-							<textarea
-								id={store.name}
-								rows={6}
-								cols={10}
-								maxLength={1000}
-								placeholder={"Hey! I just checked your website and it looks awesome! Also, I read your articles and learned a few nice tips. Thanks!"}
-								required
-								{...props}
-							/>
-							{store.error && <p>{store.error}</p>}
-						</>
-					)}
-				</Field>
-			</div>
+			<Field name="name" validate={[required<string>("Please enter you name.")]}>
+				{(store, props) => (
+					<div class={styles.item}>
+						<label for={store.name}>_{store.name}:</label>
+						<input id={store.name} placeholder={NAME} {...props} required />
+						{store.error && <p>{store.error}</p>}
+					</div>
+				)}
+			</Field>
+			<Field name="email" validate={[required<string>("Please enter your email.")]}>
+				{(store, props) => (
+					<div class={styles.item}>
+						<label for={store.name}>_{store.name}:</label>
+						<input id={store.name} type="email" placeholder={EMAIL} {...props} required />
+						{store.error && <p>{store.error}</p>}
+					</div>
+				)}
+			</Field>
+			<Field name="message" validate={[required<string>("Please enter your message.")]}>
+				{(store, props) => (
+					<div class={styles.item}>
+						<label for={store.name}>_{store.name}:</label>
+						<textarea
+							id={store.name}
+							rows={6}
+							cols={10}
+							maxLength={1000}
+							placeholder={"Hey! I just checked your website and it looks awesome! Also, I read your articles and learned a few nice tips. Thanks!"}
+							required
+							{...props}
+						/>
+						{store.error && <p>{store.error}</p>}
+					</div>
+				)}
+			</Field>
 			<div class={styles.item}>
 				<Button type="submit">submit-message</Button>
 			</div>
