50b42535b2bfb75e8dbe052d9aab5564a9c1f204
taichi221228 <taichi221228@icloud.com>
2024-06-11 09:03:20 +0900

    feat: Simplify imports and props in `<AboutMePage />` and `<Tab />`
    This commit simplifies the import statement in 'about-me' route by removing an unused '$' import from "@builder.io/qwik". The tab properties in 'about-me' route and 'Tab' component have been made simpler by removing 'onClick$' property, making the code cleaner and more manageable. It improves readability by reducing unnecessary complexity.

--- src/components/interface/editor/tab.tsx
+++ src/components/interface/editor/tab.tsx
@@ -1,29 +1,23 @@
-import { component$, type QRL } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 
 import { CloseIcon } from "~/components/interface/icons";
 
 import styles from "./tab.module.css";
 
-type Props =
-	| {
-			item: string | null;
-			onClick$?: QRL<() => void>;
-	  }
-	| {
-			item?: never;
-			onClick$?: never;
-	  };
+type Props = {
+	item?: string | null;
+};
 
 export type TabProps = Props;
 
 /** @package */
-export const Tab = component$(({ item, onClick$ }: Props) => {
+export const Tab = component$(({ item }: Props) => {
 	return (
 		<div class={styles.container}>
 			{item && (
 				<div class={styles.tab}>
 					{item}
-					<button class={styles.close} onClick$={onClick$} type="button">
+					<button class={styles.close} type="button">
 						<CloseIcon class={styles.icon} />
 					</button>
 				</div>
