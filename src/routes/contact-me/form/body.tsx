b3f95d282cd32b5205e2fcf89b20f94bb5cdc7c3
taichi221228 <taichi221228@icloud.com>
2024-06-12 11:24:04 +0900

    refactor: Rename form.tsx and related files to body.tsx
    Renamed form.tsx to body.tsx in the "contact-me" route to improve code clarity and organization. This change also includes renaming corresponding CSS module file and updating the associated import statements.

--- src/routes/contact-me/form/body.tsx
+++ src/routes/contact-me/form/body.tsx
@@ -1,9 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 
-import styles from "./form.module.css";
+import styles from "./body.module.css";
 
 /** @package */
-export const Form = component$(() => {
+export const Body = component$(() => {
 	return (
 		<div class={styles.container}>
 			<h1>Hello, scaffdog!</h1>
