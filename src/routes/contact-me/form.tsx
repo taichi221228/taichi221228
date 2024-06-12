c9943cd8b5181516eb40a42b668dc51ee7e5d3a2
taichi221228 <taichi221228@icloud.com>
2024-06-12 11:07:23 +0900

    feat: Update `<Form />` with email status tracking
    Added a 'status' signal to the contact form's email flow to track and react to the sent email's status. The form now switches its state based on the outcome of the email sending process - 'initial', 'pending', 'success', or 'fail'. This enables better user interaction and feedback.

--- src/routes/contact-me/form.tsx
+++ src/routes/contact-me/form.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import { server$ } from "@builder.io/qwik-city";
 
 import { email, maxLength, minLength, required, useForm } from "@modular-forms/qwik";
@@ -18,7 +18,7 @@ type Schema = {
 const sendEmail$ = server$(async ({ name, email, message }: Schema) => {
 	const resend = new Resend(process.env.RESEND_API_KEY);
 
-	await resend.emails.send({
+	return await resend.emails.send({
 		from: "onboarding@resend.dev",
 		to: email,
 		subject: `Hello, ${name}!`,
@@ -38,73 +38,81 @@ export const Form = component$(() => {
 		},
 	});
 
+	const status = useSignal<"initial" | "pending" | "success" | "fail">("initial");
+
 	return (
-		<Form
-			class={styles.form}
-			onSubmit$={async (values) => {
-				await sendEmail$(values);
-			}}
-		>
-			<Field
-				name="name"
-				validate={[
-					required<string>("Please enter you name."),
-					minLength(2, "Your name must have 2 at least characters."),
-					maxLength(50, "Your name must not exceed 50 characters."),
-				]}
+		status.value === "initial" && (
+			<Form
+				class={styles.form}
+				onSubmit$={async (values) => {
+					status.value = "pending";
+					const response = await sendEmail$(values);
+
+					if (response.error) status.value = "fail";
+					else status.value = "success";
+				}}
 			>
-				{(store, props) => (
-					<div class={[styles.item, store.error && styles.error]}>
-						<label for={store.name}>_{store.name}:</label>
-						<input id={store.name} required minLength={2} maxLength={50} placeholder={NAME} {...props} />
-						<p>{store.error}</p>
-					</div>
-				)}
-			</Field>
-			<Field
-				name="email"
-				validate={[
-					email("The email address is badly formatted."),
-					required<string>("Please enter your email."),
-					minLength(6, "Your name must have 6 at least characters."),
-					maxLength(254, "Your name must not exceed 254 characters."),
-				]}
-			>
-				{(store, props) => (
-					<div class={[styles.item, store.error && styles.error]}>
-						<label for={store.name}>_{store.name}:</label>
-						<input id={store.name} type="email" required min={6} maxLength={254} placeholder={EMAIL} {...props} />
-						<p>{store.error}</p>
-					</div>
-				)}
-			</Field>
-			<Field
-				name="message"
-				validate={[
-					required<string>("Please enter your message."),
-					minLength(10, "Your message must have 10 at least characters."),
-					maxLength(1000, "Your message must not exceed 1000 characters."),
-				]}
-			>
-				{(store, props) => (
-					<div class={[styles.item, store.error && styles.error]}>
-						<label for={store.name}>_{store.name}:</label>
-						<textarea
-							id={store.name}
-							required
-							rows={6}
-							cols={10}
-							maxLength={1000}
-							placeholder={"Hey! I just checked your website and it looks awesome! Also, I read your articles and learned a few nice tips. Thanks!"}
-							{...props}
-						/>
-						<p>{store.error}</p>
-					</div>
-				)}
-			</Field>
-			<div class={styles.item}>
-				<Button type="submit">submit-message</Button>
-			</div>
-		</Form>
+				<Field
+					name="name"
+					validate={[
+						required<string>("Please enter you name."),
+						minLength(2, "Your name must have 2 at least characters."),
+						maxLength(50, "Your name must not exceed 50 characters."),
+					]}
+				>
+					{(store, props) => (
+						<div class={[styles.item, store.error && styles.error]}>
+							<label for={store.name}>_{store.name}:</label>
+							<input id={store.name} required minLength={2} maxLength={50} placeholder={NAME} {...props} />
+							<p>{store.error}</p>
+						</div>
+					)}
+				</Field>
+				<Field
+					name="email"
+					validate={[
+						email("The email address is badly formatted."),
+						required<string>("Please enter your email."),
+						minLength(6, "Your name must have 6 at least characters."),
+						maxLength(254, "Your name must not exceed 254 characters."),
+					]}
+				>
+					{(store, props) => (
+						<div class={[styles.item, store.error && styles.error]}>
+							<label for={store.name}>_{store.name}:</label>
+							<input id={store.name} type="email" required min={6} maxLength={254} placeholder={EMAIL} {...props} />
+							<p>{store.error}</p>
+						</div>
+					)}
+				</Field>
+				<Field
+					name="message"
+					validate={[
+						required<string>("Please enter your message."),
+						minLength(10, "Your message must have 10 at least characters."),
+						maxLength(1000, "Your message must not exceed 1000 characters."),
+					]}
+				>
+					{(store, props) => (
+						<div class={[styles.item, store.error && styles.error]}>
+							<label for={store.name}>_{store.name}:</label>
+							<textarea
+								id={store.name}
+								required
+								rows={6}
+								cols={10}
+								maxLength={1000}
+								placeholder={"Hey! I just checked your website and it looks awesome! Also, I read your articles and learned a few nice tips. Thanks!"}
+								{...props}
+							/>
+							<p>{store.error}</p>
+						</div>
+					)}
+				</Field>
+				<div class={styles.item}>
+					<Button type="submit">submit-message</Button>
+				</div>
+			</Form>
+		)
 	);
 });
