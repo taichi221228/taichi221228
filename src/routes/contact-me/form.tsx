92a49bd8fb076a0b97bdf697510c2c4b197d5074
taichi221228 <taichi221228@icloud.com>
2024-07-02 15:18:06 +0900

    refactor: Update Status type in contact form
    The Status type in the 'contact-me' form has been updated to optionally include a 'message' field. This change broadens the functionality of the form, allowing additional information to be provided in case of success.

--- src/routes/contact-me/form.tsx
+++ src/routes/contact-me/form.tsx
@@ -7,7 +7,7 @@ import { Body } from "./form/body";
 
 import styles from "./form.module.css";
 
-type Status = { value: { name: "initial" | "pending" | "success" } } | { value: { name: "fail"; message: string } };
+type Status = { value: { name: "initial" | "pending" | "success"; message?: never } } | { value: { name: "fail"; message: string } };
 
 /** @package */
 export const STATUS = createContextId<Status>([SITENAME, "contact-me", "status"].join("."));
