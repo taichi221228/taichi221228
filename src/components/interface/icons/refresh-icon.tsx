69c09eaa745bc2c2dfa90634d78e23f647d773c4
たいち ひ <taichi221228@icloud.com>
2024-07-07 11:59:06 +0900

    feat: Add spinning animation to `<RefreshIcon />`
    A new CSS module was imported into the 'refresh-icon.tsx' file. This new module adds a spinning animation to the refresh icon. The spinning effect is created using keyframes to rotate the icon by 360 degrees infinitely.

--- src/components/interface/icons/refresh-icon.tsx
+++ src/components/interface/icons/refresh-icon.tsx
@@ -2,7 +2,9 @@ import { component$ } from "@builder.io/qwik";
 
 import { Icon, type IconProps as Props } from "./icon";
 
+import styles from "./refresh-icon.module.css";
+
 /** @package */
-export const RefreshIcon = component$(({ ...props }: Props) => {
-	return <Icon type="ri-refresh-line" {...props} />;
+export const RefreshIcon = component$(({ class: className, ...props }: Props) => {
+	return <Icon class={[className, styles.spin]} type="ri-refresh-line" {...props} />;
 });
