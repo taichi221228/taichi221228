abf0ebb493e5fd93b1725fc0bb0da8484964f2ea
たいち ひ <taichi221228@icloud.com>
2024-07-07 14:50:59 +0900

    feat: Scaffold `<SubmitButton />`
    The new component, SubmitButton has been created in the form directory. This component is intended to be used in the contact-me route. It currently contains stand-in text that will be replaced in future updates.

---
+++ src/routes/contact-me/form/submit-button.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+/** @package */
+export const SubmitButton = component$(() => {
+	return (
+		<div>
+			<h1>Hello, scaffdog!</h1>
+		</div>
+	);
+});
