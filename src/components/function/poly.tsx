c0f9586705dbf3eef3cddcdbe489b21c147a85d4
たいち ひ <taichi221228@icloud.com>
2024-07-18 16:39:43 +0900

    refactor(types): Update `COMPONENT` to `Component` in `<Poly />`
    The capitalization of the 'Component' type and its related types has been made consistent. This makes the code more readable and adheres to standard coding conventions.

--- src/components/function/poly.tsx
+++ src/components/function/poly.tsx
@@ -1,12 +1,12 @@
 import { component$, type FunctionComponent, type PropsOf, Slot } from "@builder.io/qwik";
 
-type COMPONENT = FunctionComponent | string;
+type Component = FunctionComponent | string;
 
-type Props<C extends COMPONENT = COMPONENT> = { as?: C } & PropsOf<string extends C ? "div" : C>;
+type Props<C extends Component = Component> = { as?: C } & PropsOf<string extends C ? "div" : C>;
 
-export type PolyProps<C extends COMPONENT = COMPONENT> = Props<C>;
+export type PolyProps<C extends Component = Component> = Props<C>;
 
-export const Poly = component$(<C extends COMPONENT = COMPONENT>({ as, ...props }: Props<C>) => {
+export const Poly = component$(<C extends Component = Component>({ as, ...props }: Props<C>) => {
 	const Component = as ?? "div";
 
 	return (
