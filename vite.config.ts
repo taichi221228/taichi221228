d00e6594e409f02dfee9db2ac69e67d2c6262c4e
たいち ひ <taichi221228@icloud.com>
2024-09-15 14:45:22 +0900

    chore: Add Panda CSS configuration and integrate with Qwik
    Added `panda.config.ts` for Panda CSS settings and updated Vite config to include `@builder.io/vite-plugin-macro`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import { macroPlugin } from "@builder.io/vite-plugin-macro";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig((): UserConfig => {
 	return {
 		plugins: [
+			macroPlugin({ preset: "pandacss" }),
 			qwikCity({
 				rewriteRoutes: [{ paths: { hello: "" } }],
 			}),
