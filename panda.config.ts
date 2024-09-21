3ab5ba0543f18f32dc2b19194d603eb93d16370d
たいち ひ <taichi221228@icloud.com>
2024-09-21 15:03:56 +0900

    feat: Add base color tokens to theme configuration
    Inserted a new set of base color tokens under the theme section in panda.config.ts file.

--- panda.config.ts
+++ panda.config.ts
@@ -6,6 +6,20 @@ export default defineConfig({
 	include: ["./src/**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}"],
 	exclude: [],
 	theme: {
+		tokens: {
+			colors: {
+				base: {
+					100: { value: "#fdf6e3" },
+					90: { value: "#eee8d5" },
+					70: { value: "#93a1a1" },
+					60: { value: "#839496" },
+					40: { value: "#657b83" },
+					30: { value: "#586e75" },
+					10: { value: "#073642" },
+					0: { value: "#002b36" },
+				},
+			},
+		},
 		extend: {},
 	},
 	outdir: "src/styled-system",
