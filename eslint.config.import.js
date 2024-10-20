e2f7a42f236a5db99d3fad0cc736c011446997af
たいち ひ <taichi221228@icloud.com>
2024-10-20 11:42:15 +0900

    chore(linter): Switch eslint configuration to Flat Config
    Replaced old ESLint configuration files with new flat configuration files. This transition consolidates settings, improves readability, and maintains all original rules and plugins.

---
+++ eslint.config.import.js
@@ -0,0 +1,94 @@
+/* HACK: `@types/eslint-plugin-import` is not found
+ * @ts-expect-error eslint-disable-line @typescript-eslint/ban-ts-comment */
+import importPlugin from "eslint-plugin-import";
+import importAccessPlugin from "eslint-plugin-import-access/flat-config";
+
+/**
+ * @package
+ * @type {import("eslint").Linter.FlatConfig[]}
+ */
+export const importConfigs = [
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	importPlugin.flatConfigs.recommended,
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	importPlugin.flatConfigs.typescript,
+	{
+		settings: {
+			"import/resolver": {
+				node: { extensions: [".ts", ".tsx"] },
+				typescript: { alwaysTryTypes: true },
+			},
+		},
+		rules: {
+			"import/order": [
+				"error",
+				{
+					alphabetize: { order: "asc" },
+					groups: ["builtin", "external", "internal", "parent", "sibling"],
+					"newlines-between": "always",
+					pathGroups: [
+						{
+							group: "builtin",
+							pattern: "@builder.io/qwik?(-city){,/**}",
+							position: "after",
+						},
+						{
+							group: "builtin",
+							pattern: "@qwik-*",
+							position: "after",
+						},
+						{
+							group: "external",
+							pattern: "styled-system/**",
+							position: "before",
+						},
+						{
+							group: "internal",
+							pattern: "~/constants/**",
+							position: "before",
+						},
+						{
+							group: "internal",
+							pattern: "~/routes/**",
+							position: "before",
+						},
+						{
+							group: "internal",
+							pattern: "~/components/**",
+							position: "before",
+						},
+						{
+							group: "internal",
+							pattern: "~/hooks/**",
+							position: "before",
+						},
+						{ group: "internal", pattern: "~/**" },
+						{
+							group: "sibling",
+							pattern: "./*.module.css",
+							position: "before",
+						},
+						{ group: "parent", pattern: "../**" },
+						{ group: "sibling", pattern: "./**" },
+					],
+					distinctGroup: false,
+					pathGroupsExcludedImportTypes: [],
+				},
+			],
+		},
+	},
+	{
+		plugins: { "import-access": importAccessPlugin },
+		rules: {
+			"import-access/jsdoc": [
+				"error",
+				{
+					indexLoophole: true,
+					filenameLoophole: true,
+					defaultImportability: "public",
+					treatSelfReferenceAs: "external",
+				},
+			],
+		},
+	},
+];
---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+import { config as defineConfig, configs } from "typescript-eslint";
+
+import { importConfigs } from "./eslint.config.import.js";
+import { qwikConfigs } from "./eslint.config.qwik.js";
+
+/** @type {import("eslint").Linter.FlatConfig} */
+const ignoreConfig = { ignores: ["**/dist/*", "**/node_modules/*", "**/styled-system/*"] };
+
+/** @type import("eslint").Linter.FlatConfig */
+const jsConfig = {
+	...eslint.configs.recommended,
+	rules: { "no-unused-vars": ["error", { argsIgnorePattern: "^_" }] },
+};
+
+/** @type import("typescript-eslint").ConfigWithExtends[] */
+const tsConfigs = [
+	...configs.recommendedTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			"@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+		},
+	},
+];
+
+/** @type import("eslint").Linter.FlatConfig */
+const basicConfig = {
+	languageOptions: {
+		globals: { ...globals.browser, ...globals.commonjs },
+		ecmaVersion: "latest",
+		sourceType: "module",
+		parserOptions: { ecmaFeatures: { jsx: true } },
+	},
+};
+
+/** @private */
+// HACK: Suppress TypeScript errors due to incompatibility between `eslint-plugin-import-access` and `tsc` when using `checkJS`.
+// @ts-expect-error eslint-disable-line @typescript-eslint/ban-ts-comment
+export default defineConfig(ignoreConfig, jsConfig, ...tsConfigs, basicConfig, ...importConfigs, ...qwikConfigs);
